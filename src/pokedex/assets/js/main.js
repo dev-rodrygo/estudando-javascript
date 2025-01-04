9545e7b8289ff6c294f3ce594aeb5a353d104518
dev-rodrygo <rodrigo.off29@gmail.com>
2025-01-04 14:17:18 -0300

    entendendo while e um pouco mais de css e html

--- src/pokedex/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const tutilo = document.getElementById('titulo')
-const botao = document.getElementById('inBotao')
-
-botao.addEventListener('click', function () {
-  tutilo.innerText = 'Minha Pokedex'
-  console.log('clicou no botão')
-})
